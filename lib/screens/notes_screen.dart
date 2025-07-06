3ccb7ef716671a9aa719b6c37af894fc99d735be
SMushy14 <s.mushy1@alustudent.com>
2025-07-06 20:53:23 +0200

    Modified the build context

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -5,14 +5,26 @@ import '../bloc/notes_cubit.dart';
 import '../bloc/auth_cubit.dart';
 import 'add_edit_note_dialog.dart';
 
-class NotesScreen extends StatelessWidget {
+class NotesScreen extends StatefulWidget {
   final User user;
   const NotesScreen({super.key, required this.user});
 
   @override
-  Widget build(BuildContext context) {
-    context.read<NotesCubit>().fetchNotes(user.uid);
+  State<NotesScreen> createState() => _NotesScreenState();
+}
 
+class _NotesScreenState extends State<NotesScreen> {
+  late final NotesCubit notesCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    notesCubit = context.read<NotesCubit>();
+    notesCubit.fetchNotes(widget.user.uid);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Notes'),
@@ -54,19 +66,14 @@ class NotesScreen extends StatelessWidget {
               itemBuilder: (context, index) {
                 final note = state.notes[index];
                 return Card(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 8,
-                  ),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   elevation: 4,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+                      borderRadius: BorderRadius.circular(12)),
                   child: ListTile(
-                    contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                     title: Text(note['text']),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
@@ -75,10 +82,8 @@ class NotesScreen extends StatelessWidget {
                           icon: const Icon(Icons.edit),
                           onPressed: () => showDialog(
                             context: context,
-                            builder: (_) => AddEditNoteDialog(
-                              user: user,
-                              existingNote: note,
-                            ),
+                            builder: (_) =>
+                                AddEditNoteDialog(user: widget.user, existingNote: note),
                           ),
                         ),
                         IconButton(
@@ -89,8 +94,7 @@ class NotesScreen extends StatelessWidget {
                               builder: (context) => AlertDialog(
                                 title: const Text('Confirm delete'),
                                 content: const Text(
-                                  'Are you sure you want to delete this note?',
-                                ),
+                                    'Are you sure you want to delete this note?'),
                                 actions: [
                                   TextButton(
                                     onPressed: () =>
@@ -109,11 +113,11 @@ class NotesScreen extends StatelessWidget {
                               ),
                             );
                             if (confirmed == true) {
-                              await context.read<NotesCubit>().deleteNote(
+                              await notesCubit.deleteNote(
                                 note['id'],
-                                user.uid,
+                                widget.user.uid,
                               );
-                              if (!context.mounted) return;
+                              if (!mounted) return;
                               ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
                                   content: Text("Note deleted successfully"),
@@ -135,7 +139,7 @@ class NotesScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () => showDialog(
           context: context,
-          builder: (_) => AddEditNoteDialog(user: user),
+          builder: (_) => AddEditNoteDialog(user: widget.user),
         ),
         child: const Icon(Icons.add),
       ),
